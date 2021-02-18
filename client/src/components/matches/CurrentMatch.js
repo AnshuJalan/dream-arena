88c34f4e44cf78e0005d73f5be58ee72734e3b7b
AnshuJalan <anshujalan206@gmail.com>
2021-02-18 15:44:50 +0530

    Fixed component bug

--- client/src/components/matches/CurrentMatch.js
+++ client/src/components/matches/CurrentMatch.js
@@ -1,55 +1,83 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
-import axios from 'axios';
-import Grid from "@material-ui/core/Grid"
-import Avt from "../layout/AvatarImg"
-export const Match = async ({match}) => {
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import Avt from "../layout/AvatarImg";
+
+const Match = ({ match }) => {
+  const [apiData, setApiData] = useState(null);
+
+  const { apiUrl } = match;
+
+  useEffect(() => {
+    (async () => {
       const config = {
         method: "get",
-        url:
-          `${match.apiUrl}`,
+        url: `${apiUrl}`,
       };
-    const res=await axios(config);
-    console.log(res.data);
-    return (
-        <div>
-            <Grid container spacing={3} alignItems="center" justify="center">
-                <Grid item xs={4} >
-                    {
-                        res.data.opponents.length!=0 && res.data.opponents[0].opponent.image_url ?
-                            <Avt link={res.data.opponents[0].opponent.image_url} letter={null} index={0}/>:
-                            <Avt link={null} letter={res[0][0]} index={0}/>
+      const res = await axios(config);
+      setApiData(res.data);
+    })();
+  }, [apiUrl]);
 
-                    }
-                </Grid>
-                <Grid item xs={4}>
-                </Grid>
-                <Grid item xs={4}>
-                    {
-                        res.data.opponents.length>0 && res.data.opponents[1] && res.data.opponents[1].opponent.image_url ?
-                            <Avt link={res.data.opponents[1].opponent.image_url} letter={null} index={1}/>:
-                            <Avt link={null} letter={res[res.length-1][0]} index={1}/>
+  if (apiData === null) return <p>Loading...</p>;
 
-                    }
-                </Grid>
-                <Grid item xs={12}>
-                    <h5>{res.data.name}</h5>
-                </Grid>
-                <Grid item xs={12}>
-                    <h5>{match.admin}</h5>
-                </Grid>
+  console.log(apiData);
 
-            </Grid>
-            {/* <Link to={`/user/${login}`} className='btn btn-dark'>More</Link>
+  return (
+    <div>
+      <Grid container spacing={3} alignItems="center" justify="center">
+        <Grid item xs={4}>
+          {apiData.opponents.length != 0 &&
+          apiData.opponents[0].opponent.image_url ? (
+            <Avt
+              link={apiData.opponents[0].opponent.image_url}
+              letter={null}
+              index={0}
+            />
+          ) : (
+            <Avt
+              link={null}
+              letter={apiData.opponents[0].opponent.name[0]}
+              index={0}
+            />
+          )}
+        </Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          {apiData.opponents.length > 0 &&
+          apiData.opponents[1] &&
+          apiData.opponents[1].opponent.image_url ? (
+            <Avt
+              link={apiData.opponents[1].opponent.image_url}
+              letter={null}
+              index={1}
+            />
+          ) : (
+            <Avt
+              link={null}
+              letter={apiData.opponents[0].opponent.name[0]}
+              index={1}
+            />
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          <h5>{apiData.name}</h5>
+        </Grid>
+        <Grid item xs={12}>
+          <h5>{match.admin}</h5>
+        </Grid>
+      </Grid>
+      {/* <Link to={`/user/${login}`} className='btn btn-dark'>More</Link>
             <br></br>
             <br></br> */}
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-Match.propTypes={
-    match:PropTypes.object.isRequired
-  }
+Match.propTypes = {
+  match: PropTypes.object.isRequired,
+};
 
-export default Match
+export default Match;
